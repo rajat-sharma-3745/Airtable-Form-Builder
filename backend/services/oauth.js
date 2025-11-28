47818c7e0fbe85567abd35bb50d10b867b4f9ed6
Rajat <sharmarajat3745@gmail.com>
2025-11-28 17:12:19 +0530

    fixed exchangeToken func

--- backend/services/oauth.js
+++ backend/services/oauth.js
@@ -1,19 +1,25 @@
 import axios from "axios";
 import { AIRTABLE_TOKEN_URL } from "../config/airtable.js";
 
-export async function exchangeCodeForTokens(code, redirectUri, clientId, clientSecret,codeVerifier) {
+export async function exchangeCodeForTokens(code, redirectUri, clientId, clientSecret, codeVerifier) {
   const url = AIRTABLE_TOKEN_URL;
 
+
+  const credentials = `${clientId}:${clientSecret}`
   const params = new URLSearchParams({
     grant_type: "authorization_code",
     code,
     client_id: clientId,
-    client_secret: clientSecret,
+
     redirect_uri: redirectUri,
     code_verifier: codeVerifier,
   });
 
-  const {data} = await axios.post(url, params);
+  const { data } = await axios.post(url, params, {
+    headers: {
+      Authorization: `Basic ${credentials}`
+    }
+  });
 
   return {
     accessToken: data.access_token,
@@ -30,7 +36,7 @@ export async function refreshAccessToken(refreshToken, clientId, clientSecret) {
     client_secret: clientSecret
   });
 
-  const {data} = await axios.post(AIRTABLE_TOKEN_URL, params);
+  const { data } = await axios.post(AIRTABLE_TOKEN_URL, params);
 
   return {
     accessToken: data.access_token,
