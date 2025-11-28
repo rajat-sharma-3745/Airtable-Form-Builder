fdd00588f147ac9b5450031f8874e43bf44fd7b5
Rajat <sharmarajat3745@gmail.com>
2025-11-28 08:24:15 +0530

    added form functionality

---
+++ backend/controllers/formController.js
@@ -0,0 +1,38 @@
+import User from "../models/User.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { fetchBases, fetchTables, } from '../services/airtable.js'
+import Form from "../models/Form.js";
+
+export const getBases = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req?.user?._id);
+  if (!user) return next(new ApiError('User not found', 400))
+  if (!user?.accessToken) return next(new ApiError('User not authorized', 401))
+  const bases = await fetchBases(user?.accessToken);
+
+  res.json(bases);
+})
+export const getTables = asyncHandler(async (req, res, next) => {
+  const { baseId } = req.params;
+  const user = await User.findById(req.user._id);
+  const tables = await fetchTables(user?.accessToken, baseId);
+  res.json(tables);
+})
+export const createForm = asyncHandler(async (req, res, next) => {
+  const { title, questions, airtableBaseId, airtableTableId } = req.body;
+
+  const form = await Form.create({
+    owner: req.user.userId,
+    title,
+    questions,
+    airtableBaseId,
+    airtableTableId
+  });
+
+  res.json({ message: "Form created", form });
+})
+export const getForm = asyncHandler(async (req, res, next) => {
+  const form = await Form.findById(req.params.formId);
+  if (!form) return next(new ApiError("Form not found", 404));
+  res.json(form);
+})
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
+import { auth } from "../middlewares/auth.js";
+import { createForm, getBases, getForm, getTables } from "../controllers/formController.js";
 
 
 const router = Router();
 
 
+router.get("/bases", auth, getBases);
+router.get("/:baseId/tables", auth, getTables);
 
+router.post("/", auth, createForm);
+router.get("/:formId", getForm)
 
 export default router
