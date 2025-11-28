377cc556f4bef4c8a89ea4ecbc0d4c4616aeadff
Rajat <sharmarajat3745@gmail.com>
2025-11-28 08:25:10 +0530

    added response functionality

---
+++ backend/controllers/responseController.js
@@ -0,0 +1,53 @@
+import Form from "../models/Form.js";
+import Response from "../models/Response.js";
+import User from "../models/User.js";
+import { shouldShowQuestion } from "../services/conditional.js";
+import { ApiError } from "../utils/apiError.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { validateField } from "../utils/validator.js";
+
+export const submitForm = asyncHandler(async (req, res, next) => {
+    const form = await Form.findById(req.params.formId);
+    if (!form) return next(new ApiError("Form not found", 404));
+    const answers = req.body;
+
+
+    for (const q of form.questions) {
+        const isVisible = shouldShowQuestion(q.conditionalRules, answers);
+
+        if (isVisible) {
+            const error = validateField(q, answers[q.questionKey]);
+            if (error) {
+                return res.status(400).json({ message: error });
+            }
+        }
+    }
+
+    const user = await User.findById(form.owner);
+    const airtableFields = {};
+
+    form.questions.forEach(q => {
+        airtableFields[q.label] = answers[q.questionKey];
+    });
+
+    const record = await createRecord(
+        user.accessToken,
+        form.airtableBaseId,
+        form.airtableTableId,
+        airtableFields
+    );
+
+    const response = await Response.create({
+        formId: form._id,
+        airtableRecordId: record.id,
+        answers
+    });
+
+    res.json({ message: "Response saved", response });
+
+})
+
+export const getResponses = asyncHandler(async (req, res) => {
+    const responses = await Response.find({ formId: req.params.formId });
+    res.json(responses);
+})
--- backend/routes/responseRoutes.js
+++ backend/routes/responseRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
+import { getResponses, submitForm } from "../controllers/responseController.js";
 
 
 const router = Router();
 
-
+router.post("/:formId/submit", submitForm);
+router.get("/:formId/responses", getResponses);
 
 
 export default router
