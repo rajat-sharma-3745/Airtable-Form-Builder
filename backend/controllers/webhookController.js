7933daf9585c51d16c2cc7e99bee666986551ffe
Rajat <sharmarajat3745@gmail.com>
2025-11-29 22:36:23 +0530

    fixed bug in webhook controller

--- backend/controllers/webhookController.js
+++ backend/controllers/webhookController.js
@@ -7,86 +7,90 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import User from "../models/User.js";
 
 export const webhookHanlder = async (req, res, next) => {
-  try {
-      console.log('req came')
-      res.json(200).end()
-      const raw = req.body.toString();
-      const body = JSON.parse(raw);
-      
-      const webhookId = body?.webhook?.id;
-      const baseId = body?.base?.id;
-      
-      if (!webhookId || !baseId) {
-          console.log("Heartbeat ping");
-          return;
-      }
-      
-      const webhook = await Webhook.findOne({ webhookId });
-      if (!webhook) return;
-      const user = await User.findById(webhook?.userId)
-      const sigHeader = req.headers["x-airtable-content-mac"];
-      const isValid = verifyWebhook(
-          webhook.webhookSecret,
-          raw,
-          sigHeader
-      );
-      
-      if (!isValid) {
-          console.log("Invalid signature");
-          return;
-      }
-      
-      const {data} = await axios.get(
-          `https://api.airtable.com/v0/bases/${baseId}/webhooks/${webhookId}/payloads`,
-          {
-              headers: { Authorization: `Bearer ${user?.accessToken}` },
-          }
-      );
-     const payloads = data.payloads || [];
-  
-  if (payloads.length === 0) {
-    console.log("No payloads");
-    return;
-  }
-  
-  
-  payloads.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-  
-  
-  const latest = payloads[payloads.length - 1];
-  
-  console.log("Processing latest payload:", latest.timestamp);
-  
-  
-  const tables = latest.changedTablesById || {};
-  
-  for (const tableId of Object.keys(tables)) {
-    const tableChanges = tables[tableId];
-  
-    
-    for (const recordId of Object.keys(tableChanges.changedRecordsById || {})) {
-        const rec = tableChanges.changedRecordsById[recordId];
-        console.log(rec)
-      console.log("Latest Updated:", recordId);
-  
-      await Response.findOneAndUpdate(
-        { airtableRecordId: recordId },
-        { answers: rec.current.cellValuesByFieldId, updatedAt: new Date() }
-      );
+    try {
+        console.log('req came')
+        res.json(200).end()
+        const raw = req.body.toString();
+        const body = JSON.parse(raw);
+
+        const webhookId = body?.webhook?.id;
+        const baseId = body?.base?.id;
+
+        if (!webhookId || !baseId) {
+            console.log("Heartbeat ping");
+            return;
+        }
+
+        const webhook = await Webhook.findOne({ webhookId });
+        if (!webhook) return;
+        const user = await User.findById(webhook?.userId)
+        const sigHeader = req.headers["x-airtable-content-mac"];
+        const isValid = verifyWebhook(
+            webhook.webhookSecret,
+            raw,
+            sigHeader
+        );
+
+        if (!isValid) {
+            console.log("Invalid signature");
+            return;
+        }
+
+        const { data } = await axios.get(
+            `https://api.airtable.com/v0/bases/${baseId}/webhooks/${webhookId}/payloads`,
+            {
+                headers: { Authorization: `Bearer ${user?.accessToken}` },
+            }
+        );
+        const payloads = data.payloads || [];
+
+        if (payloads.length === 0) {
+            console.log("No payloads");
+            return;
+        }
+
+
+        payloads.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+
+
+        const latest = payloads[payloads.length - 1];
+
+        console.log("Processing latest payload:", latest.timestamp);
+
+
+        const tables = latest.changedTablesById || {};
+
+        for (const tableId of Object.keys(tables)) {
+            const tableChanges = tables[tableId];
+
+
+            for (const recordId of Object.keys(tableChanges.changedRecordsById || {})) {
+                const rec = tableChanges.changedRecordsById[recordId];
+                console.log(rec)
+                console.log("Latest Updated:", recordId);
+                const cellValues = rec.current.cellValuesByFieldId;
+
+                const fieldId = Object.keys(cellValues)[0];   
+                const value = cellValues[fieldId];
+
+                await Response.findOneAndUpdate(
+                    { airtableRecordId: recordId },
+                    { answers: { ...answers, fieldId:value }, updatedAt: new Date() }
+                );
+            }
+
+
+            for (const recordId of tableChanges.destroyedRecordIds || []) {
+                console.log(" Latest Deleted:", recordId);
+
+                await Response.findOneAndUpdate(
+                    { airtableRecordId: recordId.current.cellValuesByFieldId },
+                    { deletedInAirtable: true }
+                );
+            }
+        }
+    } catch (error) {
+        console.log(error)
     }
-  
-  
-    for (const recordId of tableChanges.destroyedRecordIds || []) {
-      console.log(" Latest Deleted:", recordId);
-  
-      await Response.findOneAndUpdate(
-        { airtableRecordId: recordId.current.cellValuesByFieldId },
-        { deletedInAirtable: true }
-      );
-    }
-  }
-  } catch (error) {
-    console.log(error)
-  }
-    
+
 }
